b71f9d570d3a6264a770795ad461a3b78f232be6
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-20 13:04:27 +0300

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy } from 'react';
- import  SharedLayout from "components/SharedLayout/SharedLayout";
+import  SharedLayout from "components/SharedLayout/SharedLayout";
 import { Container } from 'components/container/Container';
 
 
