10212420c60cb219e8e9e22ee7fa842ca3eb513f
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-21 09:46:11 +0300

    list

---
+++ src/utils/castList.jsx
@@ -0,0 +1,13 @@
+import person1 from '../images/actors/actor1.jpg'
+import person2 from '../images/actors/actor2.jpg'
+import person3 from '../images/actors/actor3.jpg'
+import person4 from '../images/actors/actor4.jpg'
+import person5 from '../images/actors/actor5.jpg'
+import person6 from '../images/actors/actor6.jpg'
+import person7 from '../images/actors/actor7.jpg'
+
+export const ListMaker =( )=> {
+    return(
+      [person1, person2, person3, person4, person5, person6, person7,]
+    )
+}
